c9a68710c05c85a82a5c6c8f55a449903a2e5fda
Natalia0412 <natalia.vieiramonteiro70@gmail.com>
2021-12-27 13:54:48 -0200

    Biscoito da Sorte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      textoFrase:''
+    };
+    this.frases=['Siga seu coração','A vida é curta', 
+  'Quem acredita sempre alcança','Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.',
+'Não espere por uma crise para descobrir o que é importante em sua vida.','Se a sua vida for a melhor coisa que já te aconteceu, acredite, você tem mais sorte do que pode imaginar.',
+'Conhece-te a ti mesmo e conhecerás o universo e os deuses.','Toda a infelicidade dos homens provém da esperança.',
+'Não há nada bom nem mau a não ser estas duas coisas: a sabedoria que é um bem e a ignorância que é um mal.'];
+
+  this.quebraBiscoito=this.quebraBiscoito.bind(this);
+  }
+
+  quebraBiscoito(){
+    let state=this.state;
+    let num=Math.floor(Math.random()*this.frases.length);
+    state.textoFrase='"' + this.frases[num] + '"'
+    this.setState(state);
+
+  }
+  render(){
+    return(
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className='img'/>
+        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    );
+}
 }
 
 export default App;
